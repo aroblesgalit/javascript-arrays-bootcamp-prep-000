457fff1786069d2edaba31979d7149d226905753
Alvin Galit <aroblesgalit@gmail.com>
2018-05-16 07:10:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-const array = [1]
+const array = [1];
 
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
